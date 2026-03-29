2587d061b399aa8b26f861257de75344b60931d2
jeziel <jezielnicolascasiano@gmial.com>
2026-03-29 15:15:54 -0600

    Primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module jeziel.lacarreradelsiglo.doshipercubos {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens jeziel.lacarreradelsiglo.doshipercubos to javafx.fxml;
+    exports jeziel.lacarreradelsiglo.doshipercubos;
+}
